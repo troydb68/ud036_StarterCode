93de74efa15d1154dfe8bb498d19cc3bb292ca77
Troy Ballard <troydb68@gmail.com>
2018-01-24 05:55:24 -0600

    Autopep8 checked
    Formatted Python code to Pep8 standards

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,7 @@
 import fresh_tomatoes
 import media
 
-# Create 3 Movie instances, put them in a list, and run the fresh_tomatoes program
+# Create 3 Movie instances, add to list, and run the fresh_tomatoes program
 
 office_space = media.Movie("Office Space",
                            "Life in a Corporate America cubicle",
@@ -9,14 +9,14 @@ office_space = media.Movie("Office Space",
                            "https://www.youtube.com/watch?v=3_fG_zLbBeU")
 
 animal_farm = media.Movie("Animal Farm",
-                           "George Orwell's Book about America's future soon",
-                           "https://upload.wikimedia.org/wikipedia/en/8/85/Tt0204824.jpeg",
-                           "https://www.youtube.com/watch?v=LAeKX5n-5IE")
+                          "George Orwell's Book about America's future soon",
+                          "https://upload.wikimedia.org/wikipedia/en/8/85/Tt0204824.jpeg",
+                          "https://www.youtube.com/watch?v=LAeKX5n-5IE")
 
 rock_star = media.Movie("Rock Star",
-                       "Dream comes true but he finds out not so glamourous",
-                       "http://www.impawards.com/2001/posters/rock_star_ver1.jpg",
-                       "https://www.youtube.com/watch?v=-OTQoNvgyYs")
+                        "Dream comes true but he finds out not so glamourous",
+                        "http://www.impawards.com/2001/posters/rock_star_ver1.jpg",
+                        "https://www.youtube.com/watch?v=-OTQoNvgyYs")
 
 movies = [office_space, animal_farm, rock_star]
 
--- media.py
+++ media.py
@@ -1,9 +1,14 @@
 import webbrowser
 
-#Movie Class used to create multiple instances with title, storyline, poster image, and trailer
+# Movie Class used to create multiple instances with title, storyline, poster image, and trailer
+
+
 class Movie():
+
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        '''Constructor that requires instance variables (title, storyline, poster image, and trailer)'''
+        '''Constructor that requires instance variables
+        (title, storyline, poster image, and trailer)
+        '''
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
