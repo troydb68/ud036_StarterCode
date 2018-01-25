7db1f3e20fdf14ad01ced5f7f45b419e4cce7c3f
Troy Ballard <troydb68@gmail.com>
2018-01-25 07:55:48 -0600

    PEP8 updates
    Put # noqa at end of lines

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,17 +5,17 @@ import media
 
 office_space = media.Movie("Office Space",
                            "Life in a Corporate America cubicle",
-                           "https://upload.wikimedia.org/wikipedia/en/8/8e/Office_space_poster.jpg  # noqa",
+                           "https://upload.wikimedia.org/wikipedia/en/8/8e/Office_space_poster.jpg",  # noqa
                            "https://www.youtube.com/watch?v=3_fG_zLbBeU")
 
 animal_farm = media.Movie("Animal Farm",
                           "George Orwell's Book about America's future soon",
-                          "https://upload.wikimedia.org/wikipedia/en/8/85/Tt0204824.jpeg  # noqa",
+                          "https://upload.wikimedia.org/wikipedia/en/8/85/Tt0204824.jpeg",  # noqa
                           "https://www.youtube.com/watch?v=LAeKX5n-5IE")
 
 rock_star = media.Movie("Rock Star",
                         "Dream comes true but he finds out not so glamourous",
-                        "http://www.impawards.com/2001/posters/rock_star_ver1.jpg  # noqa",
+                        "http://www.impawards.com/2001/posters/rock_star_ver1.jpg",  # noqa
                         "https://www.youtube.com/watch?v=-OTQoNvgyYs")
 
 movies = [office_space, animal_farm, rock_star]
